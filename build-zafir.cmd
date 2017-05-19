f4fa04447ef069e7876b9d11f434d8f6cefd691a
Loic Denuziere <loic.denuziere@intellifactory.com>
2017-05-19 22:25:32 +0200

    Remove *-zafir.*, irrelevant on this branch

--- build-zafir.cmd
+++
@@ -1,8 +0,0 @@
-@ECHO OFF
-REM NOTE: This file was auto-generated with `IB.exe prepare` from `IntelliFactory.Build`.
-
-setlocal
-set PATH=%PATH%;tools\NuGet
-nuget install IntelliFactory.Build -nocache -pre -ExcludeVersion -o tools\packages
-nuget install FSharp.Compiler.Tools -nocache -version 4.0.1.21 -excludeVersion -o tools/packages
-tools\packages\FSharp.Compiler.Tools\tools\fsi.exe --exec build-zafir.fsx %*
