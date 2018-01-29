ac54cbe50e44c883a0f740175cad6b0bd04e6b1c
András Jankó <andras.janko@gmail.com>
2018-01-29 14:15:26 +0100

    Fixing build to use any prerelease WS deps

--- build.cmd
+++ build.cmd
@@ -4,4 +4,7 @@ setlocal
 .paket\paket.exe restore -g build
 if errorlevel 1 exit /b %errorlevel%
 
-paket-files\build\intellifactory\websharper\tools\WebSharper.Fake.cmd %*
+.paket\paket.exe update -g wsbuild
+if errorlevel 1 exit /b %errorlevel%
+
+paket-files\wsbuild\intellifactory\websharper\tools\WebSharper.Fake.cmd %*
--- build.sh
+++ build.sh
@@ -4,8 +4,12 @@ set -e
 
 if [ "$OS" = "Windows_NT" ]; then
     .paket/paket.exe restore -g build
+    .paket/paket.exe update -g wsbuild
+    .paket/paket.exe restore -g build
 else
+    mono .paket/paket.exe restore -g build
+    mono .paket/paket.exe update -g wsbuild
     mono .paket/paket.exe restore -g build
 fi
 
-paket-files/build/intellifactory/websharper/tools/WebSharper.Fake.sh "$@"
+paket-files/wsbuild/intellifactory/websharper/tools/WebSharper.Fake.sh "$@"
